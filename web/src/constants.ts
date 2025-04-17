e5414be19f38189b4cbdc4fe9dbd1a5d532b5ae9
Thomas Camlong <thomas@ajnart.fr>
2025-04-17 17:06:53 +0200

    Update website url

--- web/src/constants.ts
+++ web/src/constants.ts
@@ -1,5 +1,5 @@
 export const BASE_URL = "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons"
 export const REPO_PATH = "https://github.com/homarr-labs/dashboard-icons"
 export const METADATA_URL = "https://raw.githubusercontent.com/homarr-labs/dashboard-icons/refs/heads/main/metadata.json"
-export const WEB_URL = "https://icons.homarr.dev"
+export const WEB_URL = "https://dashboardicons.com"
 export const REPO_NAME = "homarr-labs/dashboard-icons"
